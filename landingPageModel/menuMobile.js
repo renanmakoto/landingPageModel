1f2714eb18ce243635f4fdcd6d6c4c3953d6e11c
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2022-07-25 17:09:34 -0300

    Add files via upload

---
+++ landingPageModel/menuMobile.js
@@ -0,0 +1,19 @@
+/*$('.menuItemsMobile i').click(function() {
+    $('.mobileMenu').fadeIn();
+})*/
+//Not in use
+
+
+let menuMobileButton = document.querySelector('.menuItemsMobile i')
+
+menuMobileButton.addEventListener('click', () => {
+    let menuItemsJS = document.querySelector('.mobileMenu')
+    if (menuItemsJS.classList.contains('show')) {
+        menuItemsJS.classList.remove('show')
+        menuItemsJS.classList.add('hide')
+    } else {
+        menuItemsJS.classList.remove('hide')
+        menuItemsJS.classList.add('show')
+    }
+})
+
