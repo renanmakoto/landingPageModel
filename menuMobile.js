52e0ebbbd3aae6717a39cb18dd12d895ce005564
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-04-09 11:10:43 -0400

    Update menuMobile.js

--- menuMobile.js
+++ menuMobile.js
@@ -2,6 +2,7 @@ let menuMobileButton = document.querySelector('.menuItemsMobile i')
 
 menuMobileButton.addEventListener('click', () => {
     let menuItemsJS = document.querySelector('.mobileMenu')
+    
     if (menuItemsJS.classList.contains('show')) {
         menuItemsJS.classList.remove('show')
         menuItemsJS.classList.add('hide')
